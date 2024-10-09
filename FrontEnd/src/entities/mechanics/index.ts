d89c3ff1ad0976656a07f8be7f78f25c6066263b
kingkiboots <tuffhr4@gmail.com>
2024-10-09 15:10:15 +0900

    style: make invaders code as consts

--- FrontEnd/src/entities/mechanics/index.ts
+++ FrontEnd/src/entities/mechanics/index.ts
@@ -1,3 +1,5 @@
+export * from "./consts/invadersConsts";
+
 export {
   flagGrunt,
   spearGrunt,
--- FrontEnd/src/entities/mechanics/model/invadersModel.ts
+++ FrontEnd/src/entities/mechanics/model/invadersModel.ts
@@ -7,47 +7,55 @@ import {
   bannerholder0,
 } from "@/shared/assets";
 import { Invader } from "../types/invader";
+import {
+  INVADERS_TYPE_CODE_FLAG_GRUNT,
+  INVADERS_TYPE_CODE_SPEAR_GRUNT,
+  INVADERS_TYPE_CODE_SWORD_GRUNT,
+  INVADERS_TYPE_CODE_MUSKETEER,
+  INVADERS_TYPE_CODE_WARRIOR,
+  INVADERS_TYPE_CODE_BATTLE_MAGE,
+} from "../consts/invadersConsts";
 
 export const flagGrunt: Invader = {
   score: 250,
   health: 500,
   icon: bucket,
-  typeCode: "G1",
+  typeCode: INVADERS_TYPE_CODE_FLAG_GRUNT,
 };
 
 export const spearGrunt: Invader = {
   score: 500,
   health: 1000,
   icon: spear,
-  typeCode: "G2",
+  typeCode: INVADERS_TYPE_CODE_SPEAR_GRUNT,
 };
 
 export const swordGrunt: Invader = {
   score: 1250,
   health: 2500,
   icon: sword,
-  typeCode: "G3",
+  typeCode: INVADERS_TYPE_CODE_SWORD_GRUNT,
 };
 
 export const musketeer: Invader = {
   score: 15000,
   health: 25000,
   icon: musketeerImg,
-  typeCode: "M",
+  typeCode: INVADERS_TYPE_CODE_MUSKETEER,
 };
 
 export const warrior: Invader = {
   score: 50000,
   health: 50000,
   icon: warriorImg,
-  typeCode: "W",
+  typeCode: INVADERS_TYPE_CODE_WARRIOR,
 };
 
 export const battleMage: Invader = {
   score: 750000,
   health: 500000,
   icon: bannerholder0,
-  typeCode: "BW",
+  typeCode: INVADERS_TYPE_CODE_BATTLE_MAGE,
 };
 
 export const INVADERS_LIST = [
